cb6bdae6d9905deb1e6d23500b590825178c11a0
Ross <RomeoKilo125@gmail.com>
2023-09-09 23:43:18 -0400

    add mongo db connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from 'express'
 
+import { db } from './config/connection.js'
+
 const PORT = process.env.PORT || 3001
 const app = express()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.listen(PORT, () => console.log(`API Server listening on port ${PORT}`))
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`API Server listening on port ${PORT}`))
+})
